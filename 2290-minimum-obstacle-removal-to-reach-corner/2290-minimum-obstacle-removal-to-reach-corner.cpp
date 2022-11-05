4329c1c5fe2444f8c987b16cb7e02d7137f8d347
Yash Mohan <62988624+YashMohanYamo2206@users.noreply.github.com>
2022-11-05 13:24:21 +0530

    Time: 859 ms (85.49%), Space: 117.2 MB (49.22%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
@@ -5,23 +5,23 @@ public:
     int minimumObstacles(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
         vector<vector<int>> dis(n, vector<int>(m, -1));
-        deque<pair<int, pair<int, int>>> dq;
-        dq.push_front({0, {0, 0}});
+        deque<pair<int, int>> dq;
+        dq.push_front({0, 0});
         dis[0][0] = 0;
         while(!dq.empty()){
             auto curr = dq.front();
             dq.pop_front();
             for(int i = 0; i < 4; ++i){
-                int r = curr.second.first + dx[i];
-                int c = curr.second.second + dy[i];
+                int r = curr.first + dx[i];
+                int c = curr.second + dy[i];
                 if(r < 0 || r >= n || c < 0 || c >= m || dis[r][c] != -1){
                     continue;
                 }
-                dis[r][c] = dis[curr.second.first][curr.second.second] + (grid[r][c] == 1);
+                dis[r][c] = dis[curr.first][curr.second] + (grid[r][c] == 1);
                 if(grid[r][c] == 1){
-                    dq.push_back({dis[r][c] ,{r, c}});
+                    dq.push_back({r, c});
                 }else{
-                    dq.push_front({dis[r][c] ,{r, c}});
+                    dq.push_front({r, c});
                 }
             }
         }
