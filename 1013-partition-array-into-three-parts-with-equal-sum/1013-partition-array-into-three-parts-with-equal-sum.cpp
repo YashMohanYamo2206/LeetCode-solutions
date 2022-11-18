71d5ead039de79e7881f6a0153d1b6b2912370df
Yash Mohan <62988624+YashMohanYamo2206@users.noreply.github.com>
2022-11-18 11:38:03 +0530

    Time: 121 ms (44.15%), Space: 32.6 MB (60.98%) - LeetHub

--- 1013-partition-array-into-three-parts-with-equal-sum/1013-partition-array-into-three-parts-with-equal-sum.cpp
+++ 1013-partition-array-into-three-parts-with-equal-sum/1013-partition-array-into-three-parts-with-equal-sum.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     bool canThreePartsEqualSum(vector<int>& arr) {
         int sum = accumulate(begin(arr), end(arr), 0);
+        int n = arr.size();
         if(sum % 3){
             return false;
         }
         int s1, s2, i1, i2;
         s1 = s2 = 0;
-        for(int i = 0; i < arr.size(); ++i){
+        for(int i = 0; i < n; ++i){
             s1 += arr[i];
             if(s1 == sum/3){
                 i1 = i;
@@ -15,10 +16,10 @@ public:
             }
         }
         
-        for(int i = 0; i < arr.size() && ((arr.size() - i - 1) - i1 > 1); ++i){
-            s2 += arr[arr.size() - i - 1];
+        for(int i = 0; i < arr.size() && ((n - i - 1) - i1 > 1); ++i){
+            s2 += arr[n - i - 1];
             if(s2 == sum/3){
-                i2 = arr.size() - i - 1;
+                i2 = n - i - 1;
                 break;
             }
         }
