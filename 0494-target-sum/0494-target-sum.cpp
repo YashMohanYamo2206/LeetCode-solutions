9168fec783e2b1da5e8c828c7338a9fec109e8a9
Yash Mohan <62988624+YashMohanYamo2206@users.noreply.github.com>
2023-02-03 15:51:53 +0530

    Time: 15 ms (76.44%), Space: 9.1 MB (66.54%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -2,14 +2,17 @@ class Solution {
 public:
 
     // int ans;
-    int dp[21][40010];
+    int dp[21][2023];
     int solve(vector<int> &nums, int target, int i , int val){
         
         if(i == nums.size()){
             return val == target;
         }
         
-         return solve(nums, target, i+1, val+nums[i]) + solve(nums, target, i+1, val-nums[i]);
+        if(dp[i][val + 1001] != -1){
+            return dp[i][val + 1001];
+        }
+        return dp[i][val + 1001] = solve(nums, target, i+1, val+nums[i]) + solve(nums, target, i+1, val-nums[i]);
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         memset(dp, -1, sizeof(dp));
