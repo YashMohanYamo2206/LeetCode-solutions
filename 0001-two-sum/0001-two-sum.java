5e066f6231a0d8ea2e125a2da34b14c2eb005a1e
Yash Mohan <62988624+YashMohanYamo2206@users.noreply.github.com>
2024-02-15 09:38:41 +0530

    Time: 2 ms (97.08%), Space: 44.6 MB (82.39%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,32 +1,13 @@
-class Solution {
-    class SortPair implements Comparator<Pair<Integer , Integer>> { 
-        // Used for sorting in ascending order of 
-        // roll number 
-        public int compare(Pair<Integer , Integer> a, Pair<Integer , Integer> b) 
-        { 
-            return a.getKey() - b.getKey(); 
-        } 
-    } 
- 
+class Solution { 
     public int[] twoSum(int[] nums, int target) {
-        List<Pair<Integer , Integer>> vals = new ArrayList<>();
+        Map<Integer, Integer> vals = new HashMap<>();
         for(int i = 0; i < nums.length; ++i){
-            vals.add(new Pair<Integer, Integer>(nums[i], i));
-        }
-        Collections.sort(vals, new SortPair());
-        int l = 0, r = nums.length - 1, sum;
-        while(l < r){
-            sum = vals.get(l).getKey() + vals.get(r).getKey();
-            if(sum == target){
-                int[] indices = {vals.get(l).getValue(), vals.get(r).getValue()};
-                return indices;
+            int comp = target - nums[i];
+            if(vals.containsKey(comp)){
+                return new int[] {i, vals.get(comp)};
             }
-            if(sum < target){
-                l++;
-            } else {
-                r--;
-            }
+            vals.put(nums[i], i);
         }
-        return nums;
+        return new int[2];
     }
 }
